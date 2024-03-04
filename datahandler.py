f5a1357a64156466d810dd1cf5dba3b0e34bb18b
AlMonther9 <ealmonzer667@gmail.com>
2024-03-04 16:12:57 +0200

    change the algorithm of data handler

--- datahandler.py
+++ datahandler.py
@@ -1,64 +1,39 @@
 import sqlite3
 import time
-# # Connect to the SQLite database (or create it if it doesn't exist)
-# conn = sqlite3.connect('data.db')
 
-# # Create a cursor object to interact with the database
-# cursor = conn.cursor()
-
-# # Create a table
-# cursor.execute('''CREATE TABLE IF NOT EXISTS users
-#               (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)''')
-
-# # Insert some data into the table
-# cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Alice', 30))
-# cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Bob', 25))
-# cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Charlie', 35))
-
-# # Commit the changes to the database
-# conn.commit()
-
-# # Query the data
-# cursor.execute("SELECT * FROM users")
-# rows = cursor.fetchall()
-# for row in rows:
-#     print(row)
-
-# # Close the connection
-# conn.close()
 
 
 class DataHandler:
     def __init__(self) -> None:
-        self.conn = sqlite3.connect('data.db')
-        self.conn.row_factory = sqlite3.Row  # Access columns by name
-        self.cursor = self.conn.cursor()
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS test
-                            (id INTEGER PRIMARY KEY, time INTEGER, gas INTEGER, bme INTEGER)''')
+        pass
 
 
     def store_data(self, data: str) -> None:
     # Parse the data string to extract the individual values
         values = data.split(' ')
         if len(values) == 2:
-            current_time = int(time.time())
+            timeInSeconds = int(time.time())
+            currentTime = time.ctime(timeInSeconds)
             gas_value = int(values[0])
             bme_value = int(values[1])
-            # Insert the data into the database
-            with self.conn:
-                self.cursor.execute("INSERT INTO test (time, gas, bme) VALUES (?, ?, ?)", (current_time, gas_value, bme_value))
+            conn = sqlite3.connect("data.db")
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO test (time, gas, bme) VALUES (?, ?, ?)", (currentTime, gas_value, bme_value))
+            conn.commit()  
+            conn.close()
+
         else:
             print("Invalid data format")
 
     def display_data(self) -> dict:
-        with self.conn:
-            self.cursor.execute("SELECT * FROM test")
-            rows = self.cursor.fetchall()
-            data = {}
-            for row in rows:
-                data[row[0]] = {
-                    'time': row[1],
-                    'gas': row[2],
-                    'bme': row[3]
-                }
-            return data
+        conn = sqlite3.connect("data.db")
+        cursor = conn.cursor()
+        rows = cursor.fetchall()
+        data = {}
+        for row in rows:
+            data[row[0]] = {
+                'time': row[1],
+                'gas': row[2],
+                'bme': row[3]
+            }
+        return data
--- serverResponse.py
+++ serverResponse.py
@@ -1,14 +1,17 @@
+from datahandler import DataHandler
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+datahandler = DataHandler()
 
 @app.route('/update', methods=['POST'])
 def update_sensor_values():
     data = request.json
-    gas_sensor_value = data.get('gasSensorValue')
-    temp_pressure_humidity_sensor_value = data.get('tempPressureHumiditySensorValue')
-    print(f"Gas Sensor value: {gas_sensor_value}")
-    print(f"Temp/Pressure/Humidity Sensor value: {temp_pressure_humidity_sensor_value}")
+    temp_pressure_humidity_sensor_value = data.get('gasSensorValue')
+    gas_sensor_value = data.get('tempPressureHumiditySensorValue')
+    reads = str(gas_sensor_value) + " " + str(temp_pressure_humidity_sensor_value)
+    datahandler.store_data(reads)
+    
     return 'Data received'
 
 if __name__ == '__main__':
