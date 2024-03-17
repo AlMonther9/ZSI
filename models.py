765858441a04c2eeb15b053527580b7034f470dc
mokh <mokhgamer323@gmail.com>
2024-03-18 00:10:53 +0200

    finish models file

--- models.py
+++ models.py
@@ -8,10 +8,14 @@ migrate = Migrate(app, db)
 
 
 class AirQuality(db.Model):
-
-    id =       db.Column(db.Integer, primary_key=True)
-    gaz =      db.Column(db.Float, nullable=False)
-    pressure = db.Column(db.Float,nullable=False)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    gaz = db.Column(db.Integer, nullable=False)
+    carbon_monoxide_ratio = db.Column(db.Float, nullable=False)
+    temp = db.Column(db.Float, nullable=False)
+    pressure = db.Column(db.Float, nullable=False)
+    humidity = db.Column(db.Float, nullable=False)
+    air_quality = db.Column(db.Float,nullable=False)
     real_time_coordinates = db.relationship('RealTimeCoordinates', backref='air_quality')
 
 class RealTimeCoordinates(db.Model):
@@ -20,7 +24,9 @@ class RealTimeCoordinates(db.Model):
     latitude = db.Column(db.Float, nullable=False)
     longitude =db.Column(db.Float, nullable=False)
     altitude = db.Column(db.Float, nullable=False)
+    google_maps = db.Column(db.Float)
     DataTime = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     air_quality_id = db.Column(db.Integer, db.ForeignKey("air_quality.id"))
+    
     def __repr__(self):
         return f"we are in {self.latitude, self.longitude} at {self.DataTime}"
