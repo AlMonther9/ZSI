34807fff07f702b0ccaaf821c8a75f8395513eb0
mokh <mokhgamer323@gmail.com>
2024-03-31 00:26:04 +0200

    updated nullable field in db

--- models.py
+++ models.py
@@ -10,22 +10,22 @@ migrate = Migrate(app, db)
 class AirQuality(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
-    gaz = db.Column(db.Integer, nullable=False)
-    carbon_monoxide_ratio = db.Column(db.Float, nullable=False)
-    temp = db.Column(db.Float, nullable=False)
-    pressure = db.Column(db.Float, nullable=False)
-    humidity = db.Column(db.Float, nullable=False)
-    air_quality = db.Column(db.Float,nullable=False)
+    gaz = db.Column(db.Integer, )
+    carbon_monoxide_ratio = db.Column(db.Float, )
+    temp = db.Column(db.Float, )
+    pressure = db.Column(db.Float, )
+    humidity = db.Column(db.Float, )
+    air_quality = db.Column(db.Float, )
     real_time_coordinates = db.relationship('RealTimeCoordinates', backref='air_quality')
 
 class RealTimeCoordinates(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude =db.Column(db.Float, nullable=False)
-    altitude = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float,)
+    longitude =db.Column(db.Float, )
+    altitude = db.Column(db.Float, )
     google_maps = db.Column(db.Float)
-    DataTime = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    DataTime = db.Column(db.DateTime, default=datetime.utcnow, )
     air_quality_id = db.Column(db.Integer, db.ForeignKey("air_quality.id"))
     
     def __repr__(self):
