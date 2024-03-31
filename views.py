c0055044a401b80e35cef8b110af08827c2672b7
mokh <mokhgamer323@gmail.com>
2024-03-31 02:00:43 +0200

    views file complete

--- views.py
+++ views.py
@@ -1,15 +1,46 @@
-from flask import request
+from flask import request,jsonify
 from settings import app
+from models import AirQuality,RealTimeCoordinates,db
 
 
 
 @app.route('/update', methods=['POST'])
 def update_sensor_values():
-    data = request.json
-    temp_pressure_humidity_sensor_value = data.get('gasSensorValue')
-    gas_sensor_value = data.get('tempPressureHumiditySensorValue')
-    reads = str(gas_sensor_value) + " " + str(temp_pressure_humidity_sensor_value)
-    # datahandler.store_data(reads)
+    json_data = request.json
+    query_params = request.args.to_dict()
+    data = {**json_data, **query_params}
+
     
-    return 'Data received'
+    # Create AirQuality instance
+    air_quality = AirQuality(
+        gaz=data.get('gaz'),
+        carbon_monoxide_ratio=data.get('carbon_monoxide_ratio'),
+        temp=data.get('temp'),
+        pressure=data.get('pressure'),
+        humidity=data.get('humidity'),
+        air_quality=data.get('air_quality')
+    )
+
+    db.session.add(air_quality)
+    db.session.flush()
+    
+    # Create RealTimeCoordinates instance
+    real_time_coordinates = RealTimeCoordinates(
+        latitude=data.get('latitude'),
+        longitude=data.get('longitude'),
+        altitude=data.get('altitude'),
+        google_maps=data.get('google_maps'),
+        
+        air_quality_id=air_quality.id
+    )
+    db.session.add(real_time_coordinates)
+
+    try:
+        db.session.commit()
+
+        return jsonify(data),200
+    # if there any errors undo the changes        
+    except Exception as e :
+        db.session.rollback()
+        return f"Failed to commit data: {str(e)}", 500
 
